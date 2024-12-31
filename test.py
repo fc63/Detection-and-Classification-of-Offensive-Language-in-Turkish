c2d4e66cf62bef1b180aa433f9c5c642d97c88fc
Furkan Çoban <139001749+fc63@users.noreply.github.com>
2024-12-31 16:14:17 +0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,29 @@
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+import torch
+
+repo_id = "fc63/turkish-toxic-language-detection"
+
+model = AutoModelForSequenceClassification.from_pretrained(repo_id)
+tokenizer = AutoTokenizer.from_pretrained(repo_id)
+
+class_labels = {
+    0: "Non-Toxic",
+    1: "Toxic"
+}
+
+test_sentences = [
+    "Bu çok aptalca bir hareketti!",  # Toxic
+    "Bugün hava oldukça güzel.",      # Non-Toxic
+    "Sen nasıl bir piç kurususun lan",  # Toxic
+    "Kürtler gerizekalı.",      # Toxic
+    "Kadınlar araba kullanmayı beceremez.", # Toxic
+    "Bitti demediniz mi", # Non-Toxic
+]
+
+for sentence in test_sentences:
+    inputs = tokenizer(sentence, return_tensors="pt", truncation=True, padding=True)
+    outputs = model(**inputs)
+    logits = outputs.logits
+    predicted_class = torch.argmax(logits, dim=1).item()
+    print(f"Test sentence: {sentence}")
+    print(f"Predicted class: {predicted_class} ({class_labels[predicted_class]})\n")
